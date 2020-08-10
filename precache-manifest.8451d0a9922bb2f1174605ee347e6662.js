45caf639d529d2badea4844eba7d7a3c1c10cdfb
Gaurav Thakur <gthakur581@gmail.com>
2020-08-10 11:02:49 +0530

    Updates

---
+++ precache-manifest.8451d0a9922bb2f1174605ee347e6662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7eaad49779f1bb75b00d133037e378c",
+    "url": "/clister-react-app/index.html"
+  },
+  {
+    "revision": "a8380f61dfdb930c0df0",
+    "url": "/clister-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3701767a1546e81a53f4",
+    "url": "/clister-react-app/static/js/2.6cf4d752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clister-react-app/static/js/2.6cf4d752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8380f61dfdb930c0df0",
+    "url": "/clister-react-app/static/js/main.c93f7a06.chunk.js"
+  },
+  {
+    "revision": "0be5e4626e9bad4f8fa9",
+    "url": "/clister-react-app/static/js/runtime-main.36fe076c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clister-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
